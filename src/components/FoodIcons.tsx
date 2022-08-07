ccbe06ba29d8032e10a42612a62aeada065f6b85
Jordan Wells <jordantwells@gmail.com>
2022-08-06 22:55:18 -0400

    mvp no animation

--- src/components/FoodIcons.tsx
+++ src/components/FoodIcons.tsx
@@ -30,16 +30,16 @@ export default function FoodIcons({
     "Dim Sum", 
   ];
   return (
-    <div className="w-full h-full grid grid-rows-2  grid-flow-col overflow-x-scroll overflow-y-hidden pt-10 pl-4 ">
+    <div className="w-full h-full grid grid-rows-2  grid-flow-col overflow-x-scroll overflow-y-hidden pt-10 pl-5 md:pl-10 ">
       {foods.map((food, index) => (
-        <div className="w-20 h-20 hover:cursor-pointer hover:bg-blue-200 hover:rounded-2xl p-2 flex flex-col items-center justify-center" key={index}>
+        <div className="w-20 h-20 hover:cursor-pointer hover:bg-blue-50 hover:rounded-2xl p-1 flex flex-col items-center justify-center" key={index}>
           <img
             src={`/${food}.svg`}
             alt={food}
             className="h-full aspect-square"
             onClick={() => (setFoodQuery(food), setToggle(false))}
           />
-          <p className="text-xs font-bold">{food}</p>
+          <p className="text-sm font-bold">{food}</p>
         </div>
       ))}
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,72 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head>
+    <title>Where should I eat? | whereshouldieat.app</title>
+    <meta
+      name='viewport'
+      content='width=device-width, initial-scale=1'
+    ></meta>
+    <meta
+      property='og:description'
+      content="Put in where you are and what you're craving, and we'll tell you where to eat."
+    />
+    <meta
+      name='description'
+      content="Put in where you are and what you're craving, and we'll tell you where to eat."
+    />
+    <meta property='og:title' content='Where should I eat? | whereshouldieat.app' key='title' />
+    <meta
+      property='og:image'
+      content={
+        '/jordan.webp'
+      }
+    />
+    <meta property='og:url' content={'https://whereshouldieat.app'} />
+    <meta name='twitter:card' content='summary_large_image'></meta>
+    <meta name='twitter:site' content='@jordantwells42' />
+    <meta name='twitter:creator' content='@jordantwells42' />
+    <meta name='twitter:title' content='Where should I eat? | whereshouldieat.app' />
+    <meta
+      name='twitter:description'
+      content="Put in where you are and what you're craving, and we'll tell you where to eat."
+    />
+    <meta
+      name='twitter:image'
+      content='/jordan.webp'
+    />
+    <link rel="canonical" href={'https://whereshouldieat.app'} />
+    <script type='application/ld+json'>{`
+{
+"@context": "https://schema.org/",
+"@type": "Person",
+"name": "Jordan Wells",
+"url": "https://jordantwells.com",
+"image": "",
+"sameAs": [
+"https://www.linkedin.com/in/jordantwells/",
+"https://github.com/jordantwells42"
+],
+"jobTitle": "Student",
+"worksFor": {
+"@type": "Organization",
+"name": "University of Texas at Austin"
+}  
+}
+`}</script>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+  </Head>
+  <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp;
