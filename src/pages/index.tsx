6a89bded03527093964e44cc58f19d5517160fae
Jordan Wells <jordantwells@gmail.com>
2022-08-07 15:52:54 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -328,7 +328,7 @@ useEffect(() => {
     <>
       <div
         style={{ backgroundBlendMode: toggle ? "darken" : "" }}
-        className="relative flex h-full w-full flex-col items-center justify-center bg-stone-700  font-main"
+        className="relative flex h-full min-h-screen w-full flex-col items-center justify-center bg-stone-700  font-main"
       >
         {/*TINDER*/}
         <div className="relative flex h-full w-full flex-col items-center justify-center overflow-x-hidden overflow-y-hidden">
